dbc37619ca4e48f91e3c2dc6d7bfc55b721d8338
Djatiaja <djamgt23@gmail.com>
2025-09-24 16:49:49 +0700

    feat(middleware): add schemavalidation using zod

--- src/api/v1/middlewares/schema.middleware.ts
+++ src/api/v1/middlewares/schema.middleware.ts
@@ -1,9 +1,29 @@
-import z from "zod";
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import { ZodError, ZodSchema } from "zod";
+import { sendResponse } from "../helpers/baseResponse";
 
-const userSchema = z.object({
-    name: z.string().min(1, "Name is required"),
-    email: z.email("Invalid email format"),
-    password: z.string().min(8, "Password must be at least 8 characters long"),
-    username: z.string().optional(),
-    profileImage: z.url("Invalid URL format").optional(),
-});
+export const validateBody =
+    (schema: ZodSchema) => (request: Request, response: Response, next: NextFunction) => {
+        try {
+            schema.parse(request.body);
+            next();
+        } catch (err: unknown) {
+            if (err instanceof ZodError) {
+
+                // Group issues by field
+                const errors: Record<string, string[]> = {};
+
+                err.issues.forEach(issue => {
+                    const field = issue.path.join(".") || "global";
+                    if (!errors[field]) {
+                        errors[field] = [];
+                    }
+                    errors[field].push(issue.message);
+                });
+
+                sendResponse({
+                    res: response, statusCode: 500, success: false, message: "Request validation fail", data: null, errors: errors
+                });
+            }
+        }
+    };
