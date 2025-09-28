1fa88faed6daa06976a7218210b15d982b7ad112
Djatiaja <djamgt23@gmail.com>
2025-09-28 11:50:09 +0700

    feat(cors): add cors url validation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
 import router from "./api/v1/routes/index.route";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+const allowedOrigins = [
+    "http://localhost:3000",     // Dev
+];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS ❌"));
+            }
+        },
+        credentials: true,
+    })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", router);
