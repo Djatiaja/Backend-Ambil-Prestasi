c8818667501c73e7136643f7e08aa803650bd609
Djatiaja <djamgt23@gmail.com>
2025-10-29 23:29:36 +0700

    feat(material file ): change file type from image to pdf

--- src/api/v1/schemas/materialFile.schema.ts
+++ src/api/v1/schemas/materialFile.schema.ts
@@ -1,22 +1,18 @@
 import { z } from "zod";
-import { imageSchema } from "./image.schema";
 
-export const pdfSchema = z.object({
-    file: z
-        .any()
-        .refine((file) => !!file, "File harus disertakan.")
-        .refine((file) => ["application/pdf"].includes(file.mimetype), {
-            message: "Format file tidak valid. Gunakan PDF.",
-        })
-        .refine((file) => file.size <= 100 * 1024 * 1024, {
-            message: "Ukuran file maksimal 100MB.",
-        }),
-});
+export const pdfSchema = z.any()
+    .refine((file) => !!file, "File harus disertakan.")
+    .refine((file) => ["application/pdf"].includes(file.mimetype), {
+        message: "Format file tidak valid. Gunakan PDF.",
+    })
+    .refine((file) => file.size <= 100 * 1024 * 1024, {
+        message: "Ukuran file maksimal 100MB.",
+    });
 
 
 export const createMaterialFileSchema = z.object({
     title: z.string().min(3, "Judul minimal 3 karakter."),
-    file: imageSchema
+    file: pdfSchema
 });
 
 export const updateMaterialFileSchema = createMaterialFileSchema.partial();
