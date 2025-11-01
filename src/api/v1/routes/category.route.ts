2f9581606d3b60371cfc4e5c71a584f2ed56a83d
Djatiaja <djamgt23@gmail.com>
2025-11-01 08:55:13 +0700

    feat(category): add get access to teacher

--- src/api/v1/routes/category.route.ts
+++ src/api/v1/routes/category.route.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import categoryController from "../controllers/category.controller";
 import { validateBody } from "../middlewares/schema.middleware";
 import { createCategorySchema, updateCategorySchema } from "../schemas/category.schema";
+import { verifyRole } from "../middlewares/verifyrole.middleware";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/", categoryController.getAllCategories);
-categoryRouter.get("/:id", categoryController.getCategoryById);
-categoryRouter.post("/", validateBody(createCategorySchema), categoryController.createCategory);
-categoryRouter.patch("/:id", validateBody(updateCategorySchema), categoryController.updateCategory);
-categoryRouter.delete("/:id", categoryController.deleteCategory);
+categoryRouter.get("/:id", verifyRole(["Admin"]), categoryController.getCategoryById);
+categoryRouter.post("/", verifyRole(["Admin"]), validateBody(createCategorySchema), categoryController.createCategory);
+categoryRouter.patch("/:id", verifyRole(["Admin"]), validateBody(updateCategorySchema), categoryController.updateCategory);
+categoryRouter.delete("/:id", verifyRole(["Admin"]), categoryController.deleteCategory);
 
 export default categoryRouter;
